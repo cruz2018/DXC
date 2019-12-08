ce3419c40493c24b1000eb9a8c8192cac40b0ee0
Marcus <marcusfunctional01@gmail.com>
2019-12-08 22:34:36 +0100

    added all functions

--- loginPageautomation.py
+++ loginPageautomation.py
@@ -11,3 +11,19 @@ class LoginTest(unittest.TestCase):
 	@classmethods
 	def teardownClass(cls):
 		cls.driver.quit()
+
+	@classmethods
+	def setUp(self):
+		self.driver.get("http://travel.agileway.net")
+
+	@classmethds
+	def tearDown(self):
+		self.driver.find_element_by_link_text('Sign off').click()
+
+	def test_first_case(self):
+		self.assertEqual('Agile Travel', self.driver.title)
+		self.driver.find_element_by_name('username').send_keys('agileway')
+
+	def test_second_case(self):
+		self.driver.find_element_by_id('register_link').click()
+		self.assertIn('register', self.driver.find_element_by_tag_name('body').text)
